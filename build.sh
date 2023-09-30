ed714e71b6f2953732bc672dd7330cb78fd128d6
pexcn <pexcn97@gmail.com>
2023-09-30 16:07:13 +0800

    initial

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# shellcheck disable=SC3043,SC2086,SC2164,SC2103,SC2046
+
+get_sources() {
+  local repo_url="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY_OWNER}/openwrt-ipq60xx.git"
+  local branch="$GITHUB_REF_NAME"
+  git clone $repo_url --single-branch -b $branch openwrt-source
+}
+
+build_firmware() {
+  cd openwrt-source
+
+  ./scripts/feeds update -a
+  ./scripts/feeds install -a
+
+  cp ${GITHUB_WORKSPACE}/configs/${BUILD_PROFILE} .config
+  make -j$(nproc) V=w || make -j1 V=sc
+
+  cd -
+}
+
+package_binaries() {
+  local bin_dir="openwrt-source/bin"
+  local tarball="${BUILD_PROFILE}.tar.gz"
+  tar -zcvf $tarball -C $bin_dir $(ls $bin_dir -1)
+}
+
+get_sources
+build_firmware
+package_binaries
